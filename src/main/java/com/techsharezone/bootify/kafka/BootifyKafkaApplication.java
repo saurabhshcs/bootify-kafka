799c1e4a9ff645e3e8618a70e60b0d9e6bbd9a63
Saurabh Sharma <saurabhshcs@gmail.com>
2021-01-27 23:27:36 +0000

    [TSZ-Spring-Boot-Kafka-001] saurabhshcs - Initial project setup

---
+++ src/main/java/com/techsharezone/bootify/kafka/BootifyKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootify.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifyKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifyKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techsharezone/bootify/kafka/BootifyKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsharezone.bootify.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootifyKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
