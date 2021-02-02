14e04249df9f4a565c0ae47f541c2c5e5836273e
SAURABH SHARMA <saurabhshcs@gmail.com>
2021-02-02 23:35:54 +0000

    [TSZ-Spring-Boot-Kafka-001] saurabhshcs - Disc-configure topic and producer configuration for testing reasons.

--- src/main/java/com/techsharezone/bootify/kafka/config/KafkaProducerConfig.java
+++ src/main/java/com/techsharezone/bootify/kafka/config/KafkaProducerConfig.java
@@ -6,34 +6,25 @@ package com.techsharezone.bootify.kafka.config;
  * @author  saurabhshcs
  */
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
 
-    @Bean
-    public ProducerFactory<String, String> producerFactory(final String bootstrapAddress) {
-
-        Map<String, Object> configsProps = new HashMap<>();
-        configsProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        configsProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configsProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
-        return new DefaultKafkaProducerFactory<>(configsProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate() {
-
-        return new KafkaTemplate<>(producerFactory(""));
-    }
+//    @Bean
+//    public ProducerFactory<String, String> producerFactory(final String bootstrapAddress) {
+//
+//        Map<String, Object> configsProps = new HashMap<>();
+//        configsProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+//        configsProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        configsProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//
+//        return new DefaultKafkaProducerFactory<>(configsProps);
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, String> kafkaTemplate() {
+//
+//        return new KafkaTemplate<>(producerFactory(""));
+//    }
 }
--- src/main/java/com/techsharezone/bootify/kafka/config/KafkaTopicConfig.java
+++ src/main/java/com/techsharezone/bootify/kafka/config/KafkaTopicConfig.java
@@ -6,30 +6,22 @@ package com.techsharezone.bootify.kafka.config;
  * @author  saurabhshcs
  */
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaTopicConfig {
 
-    @Value(value = "{$kafka.bootsrapAddress}")
-    private String bootStrapAddress;
-
-    public KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapAddress);
-        return new KafkaAdmin(configs);
-    }
-
-    @Bean
-    public NewTopic topic1() {
-        return new NewTopic("techsharezone", 1, (short) 1);
-    }
+//    @Value(value = "{$kafka.bootsrapAddress}")
+//    private String bootStrapAddress;
+//
+//    public KafkaAdmin kafkaAdmin() {
+//        Map<String, Object> configs = new HashMap<>();
+//        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapAddress);
+//        return new KafkaAdmin(configs);
+//    }
+//
+//    @Bean
+//    public NewTopic topic1() {
+//        return new NewTopic("techsharezone", 1, (short) 1);
+//    }
 }
